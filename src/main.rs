20231a5b1b981bd83a4445df224b922fa4a9a5c4
Janus <janus@insignificancegalore.net>
2021-10-30 00:07:17 +0200

    Add commands called from js

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
+
+struct MyState {
+    count: usize
+}
+
+#[derive(serde::Serialize)]
+struct CustomResponse {
+  message: String,
+  other_val: usize,
+}
+
+#[tauri::command]
+fn my_custom_command() {
+  println!("I was invoked from JS!");
+}
+
+#[tauri::command]
+fn elaborate_command(
+    window: tauri::Window,
+    message: String, 
+    state: tauri::State<'_, MyState>,
+  ) -> Result<CustomResponse, String> {
+    println!("Called from {} with message \"{}\"", window.label(), &message);
+    // state.count+=1;  // wrap in mutex for mutability
+    Ok(CustomResponse{message, other_val: state.count})
+  }
+
 fn main() {
+    let state = MyState{count: 0};
     tauri::Builder::default()
+        .manage(state)
+        .invoke_handler(tauri::generate_handler![my_custom_command, elaborate_command])
         .run(tauri::generate_context!())
         .expect("unable to run Tauri application");
 }
